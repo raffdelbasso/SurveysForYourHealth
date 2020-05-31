cae7d656382093d024099cd1d8a6c96499236df2
Raffaele Del Basso <raffydelbasso@gmail.com>
2020-05-31 11:56:05 +0200

    Aggiunta la rimozione delle stored procedures prima della loro creazione

--- sql/storedProcedures.sql
+++ sql/storedProcedures.sql
@@ -1,6 +1,7 @@
 use dbpsicologhe;
 
 DELIMITER $$
+drop procedure if exists effettuaLogin$$
 create procedure if not exists effettuaLogin (in email varchar(255), in password char(32))
 select *
 from utenti
@@ -9,6 +10,7 @@ where utenti.email = email and
 DELIMITER ;
 
 DELIMITER $$
+drop procedure if exists cercaUtentePerEmail$$
 create procedure if not exists cercaUtentePerEmail (in email varchar(255))
 select *
 from utenti
@@ -16,12 +18,14 @@ where utenti.email = email$$
 DELIMITER ;
 
 DELIMITER $$
+drop procedure if exists registraUtente$$
 create procedure if not exists registraUtente (in cognome varchar(50), in nome varchar(50), in email varchar(255), in password char(32), in tipoUtente int, in codPediatra int)
 insert into utenti(utenti.cognome, utenti.nome, utenti.email, utenti.password, utenti.tipoUtente, utenti.codPediatra) values
 (cognome, nome, email, password, tipoUtente, codPediatra)$$
 DELIMITER ;
 
 DELIMITER $$
+drop procedure if exists modificaUtente$$
 create procedure if not exists modificaUtente (in idUtente int, in nuovoCognome varchar(50), in nuovoNome varchar(50), in nuovaEmail varchar(255), in nuovaPassword char(32), in nuovoPediatra int)
 update utenti
 set cognome = nuovoCognome,
@@ -33,6 +37,7 @@ where utenti.idUtente = idUtente$$
 DELIMITER ;
 
 DELIMITER $$
+drop procedure if exists mostraPediatri$$
 create procedure if not exists mostraPediatri ()
 select *
 from utenti
@@ -40,12 +45,14 @@ where tipoUtente = 3$$
 DELIMITER ;
 
 DELIMITER $$
+drop procedure if exists mostraQuestionari$$
 create procedure if not exists mostraQuestionari ()
 select *
 from questionari$$
 DELIMITER ;
 
 DELIMITER $$
+drop procedure if exists cercaQuestionarioPerId$$
 create procedure if not exists cercaQuestionarioPerId(in idQuestionario int)
 select *
 from questionari
@@ -53,6 +60,7 @@ where questionari.idQuestionario = idQuestionario$$
 DELIMITER ;
 
 DELIMITER $$
+drop procedure if exists mostraDomande$$
 create procedure if not exists mostraDomande (in idQuestionario int)
 select *
 from questionari, domande
@@ -61,6 +69,7 @@ where questionari.idQuestionario = codQuestionario and
 DELIMITER ;
 
 DELIMITER $$
+drop procedure if exists mostraOpzioni$$
 create procedure if not exists mostraOpzioni (in idDomanda int)
 select idOpzioneInDomanda, opzioni.testo, punteggio
 from opzioni, domande, opzioniInDomande
@@ -70,30 +79,35 @@ where codDomanda = domande.idDomanda and
 DELIMITER ;
 
 DELIMITER $$
+drop procedure if exists inserisciQuestionario$$
 create procedure if not exists inserisciQuestionario (in nome varchar(100))
 insert into questionari(questionari.nome) values
 (nome)$$
 DELIMITER ;
 
 DELIMITER $$
+drop procedure if exists inserisciDomanda$$
 create procedure if not exists inserisciDomanda (in testo varchar(255), in immagine varchar(255), in codQuestionario int)
 insert into domande(domande.testo, domande.immagine, domande.codQuestionario) values
 (testo, immagine, codQuestionario)$$
 DELIMITER ;
 
 DELIMITER $$
+drop procedure if exists inserisciOpzione$$
 create procedure if not exists inserisciOpzione (in testo varchar(50), in punteggio int, in codDomanda int)
 insert into opzioni(opzioni.testo, opzioni.punteggio, opzioni.codDomanda) values
 (testo, punteggio, codDomanda)$$
 DELIMITER ;
 
 DELIMITER $$
+drop procedure if exists registraRisposta$$
 create procedure if not exists registraRisposta(in dataOra datetime, in codFiglio int, in codOpzioneInDomanda int)
 insert into risposte(risposte.dataOra, risposte.codFiglio, risposte.codOpzioneInDomanda) values
 (dataOra, codFiglio, codOpzioneInDomanda)$$
 DELIMITER ;
 
 DELIMITER $$
+drop procedure if exists numeroFigli$$
 create procedure if not exists numeroFigli(in idUtente int)
 select count(*)
 from utenti, figli
@@ -101,6 +115,7 @@ where utenti.idUtente = codUtente and utenti.idUtente = idUtente$$
 DELIMITER ;
 
 DELIMITER $$
+drop procedure if exists mostraFigli$$
 create procedure if not exists mostraFigli(in idUtente int)
 select idFiglio, figli.nome, figli.cognome
 from utenti, figli
@@ -108,12 +123,14 @@ where utenti.idUtente = codUtente and utenti.idUtente = idUtente$$
 DELIMITER ;
 
 DELIMITER $$
+drop procedure if exists registraFiglio$$
 create procedure if not exists registraFiglio(in cognome varchar(50), in nome varchar(50), in dataNascita date, in codUtente int)
 insert into figli(figli.cognome, figli.nome, figli.dataNascita, figli.codUtente) values
 (cognome, nome, dataNascita, codUtente)$$
 DELIMITER ;
 
 DELIMITER $$
+drop procedure if exists verificaPassword$$
 create procedure if not exists verificaPassword(in idUtente int, in password char(32))
 select *
 from utenti
@@ -122,17 +139,21 @@ where utenti.idUtente = idUtente and
 DELIMITER ;
 
 DELIMITER $$
-create procedure if not exists calcolaPunteggioCritico(in dataOra datetime)
+drop procedure if exists calcolaPunteggioCritico$$
+create procedure if not exists calcolaPunteggioCritico(in idFiglio int, in dataOra datetime)
 select sum(punteggio)
 from risposte, opzioniInDomande
 where idOpzioneInDomanda = codOpzioneInDomanda and
-	  codDomanda in(2, 7, 9, 13, 14, 15) and risposte.dataOra = dataOra$$
+	  codDomanda in(2, 7, 9, 13, 14, 15) and risposte.dataOra = dataOra and
+	  risposte.codFiglio = idFiglio$$
 DELIMITER ;
 
 DELIMITER $$
-create procedure if not exists calcolaPunteggioTotale(in dataOra datetime)
+drop procedure if exists calcolaPunteggioTotale$$
+create procedure if not exists calcolaPunteggioTotale(in idFiglio int, in dataOra datetime)
 select sum(punteggio)
 from risposte, opzioniInDomande
 where idOpzioneInDomanda = codOpzioneInDomanda and
-	  risposte.dataOra = dataOra$$
+	  risposte.dataOra = dataOra and
+	  risposte.codFiglio = idFiglio$$
 DELIMITER ;
